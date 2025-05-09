de6f61f7aba788ba3cdf9786138018335c72266d
Jiaming Sec <jiamingqu0728@gmail.com>
2025-05-09 00:20:43 -0400

    Update loop.py

--- loop.py
+++ loop.py
@@ -18,3 +18,15 @@ a = 33
 b = 200
 if b > a:
 print("b is greater than a") # you will get an error
+
+# Elif
+# "elif": if the previous conditions were not true, then try this condition
+a = 33
+b = 200
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+
+# Else
+# 
