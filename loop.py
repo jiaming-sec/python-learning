535a8862c7d72663bb606ec4fff2aea4bfb118e1
Jiaming Sec <jiamingqu0728@gmail.com>
2025-05-09 00:57:25 -0400

    Update loop.py

--- loop.py
+++ loop.py
@@ -107,6 +107,15 @@ match day:
   case _:
     print("Looking forward to the Weekend")
 
+# Combine Values
+# use pipe character "|" as an "or" operator in the case evaluation to check
+day = 4
+match day:
+  case 1 | 2 | 3 | 4 | 5:
+    print("Today is a weekday")
+  case 6 | 7:
+    print(":Weekends")
+
 
 
 
