1b0e5a696ff66d2fcd67a49862d443dc3702ddd1
Jiaming Sec <jiamingqu0728@gmail.com>
2025-05-09 00:38:40 -0400

    Update loop.py

--- loop.py
+++ loop.py
@@ -29,4 +29,43 @@ elif a == b:
   print("a and b are equal")
 
 # Else
-# 
+# "else": catches anything which isn't caught by the preceding condition.
+a = 33
+b = 200
+if b > a:
+  print("b is greater than a")
+elif a ==b :
+  print("a and b are equal")
+else:
+  print("a is greater than b")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
