edc6ba72fe7f568a1f7e0a3f24acf84702510b67
Jiaming Sec <jiamingqu0728@gmail.com>
2025-05-09 01:40:47 -0400

    Update loop.py

--- loop.py
+++ loop.py
@@ -1,1 +1,9 @@
+# Python has two primitive loop
+while loops
+for loops
 
+# while loop
+# with "while" loop, we can execute a set of statements as long as a condition is true
+# print i as long as i is less than 6
+i = 1
+while 
