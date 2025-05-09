b88fdc2ef02497d3d6b9689cc72c6bc981f9045c
Jiaming Sec <jiamingqu0728@gmail.com>
2025-05-09 00:54:14 -0400

    Update loop.py

--- loop.py
+++ loop.py
@@ -68,7 +68,33 @@ b = 200
 if b > a:
   pass
 
+# "match" statement is used to perform different actions based on different conditions
+# Instead of writing many if...else statements, you can use "match" statement.
 
+match expression:
+  case x:
+    code block
+  case y:
+    code block
+  case z:
+    code block
+
+day = 4
+match day:
+  case 1:
+    print("Monday")
+  case 2:
+    print("Tuesday")
+  case 3:
+    print("Wednesday")
+  case 4:
+    print("Thursday")
+  case 5:
+    print("Friday")
+  case 6:
+    print("Saturday")
+  case 7:
+    print("Sunday")
 
 
 
