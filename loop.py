0091f680d4b3de3c376487f694ca2ac0b66ac812
Jiaming Sec <jiamingqu0728@gmail.com>
2025-05-09 01:48:16 -0400

    Update loop.py

--- loop.py
+++ loop.py
@@ -28,3 +28,11 @@ while i < 6:
   if i == 3:
     continue
   print(i) # 1 2 4 5 6
+
+# with "else" statement we can run a block of code once when the condition no longer is true:
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+else: 
+  print("i is no longer less than 6")
