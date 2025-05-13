a3ef7b6b2de69fa1e41fcb1989458ac0ae8d2f52
Jiaming Sec <jiamingqu0728@gmail.com>
2025-05-13 00:52:48 -0400

    Update loop.py

--- loop.py
+++ loop.py
@@ -75,12 +75,36 @@ for x in range(6):
 
 # range(6) is not the values of 0 to 6, but the values 0 to 5
 # range(2,6), which means vales from 2 to 6 (not including 6)
+for x in range(2, 6):
+  print(x)
 
+# to specify the increment value by adding a thrid parameter: "range(2, 30 , 3):"
+# increment the sequence with 3 (default is 1)
+for x in range(2, 30, 3):
+  print(x)
 
+# "else" keyword in a "for" loop specifies a block of code to be executed when the loop is finished
+for x in range(6):
+  print(x)
+else:
+  print("Finished!")
 
+# "else" will NOT be executed if the loop is stopped by a break statment.
+# break the loop whe nx is 3, and see what happens with "else block
+for x in range (6):
+  if x == 3: break
+    print(x)
+else:
+print("Finished")
 
+# "nested" loop
+for x in adj:
+  for y in fruits:
+    print(x,y)
 
-
+# "pass" to have a for loop with no content
+for x in [0,1,2]:
+  pass
 
 
 
