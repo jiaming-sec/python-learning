d9ba1e29155a27a881eae6b1dc5dff1defbb4f89
Jiaming Sec <jiamingqu0728@gmail.com>
2025-05-14 15:55:09 -0400

    Update function.py

--- function.py
+++ function.py
@@ -17,3 +17,14 @@ my_function()
 
 # Recursion
 # Function recursion means defined function can call itself
+def tri_recursion(k):
+  if k > 0 :
+    result = k + tri_recursion(k-1)
+    print(result)
+  else:
+    result = 0
+
+return result
+
+print("Recursion example results:")
+tri_recursion(6)
