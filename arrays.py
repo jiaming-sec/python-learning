99de94cca504e5c4422a72fec9110035a2ca1848
Jiaming Sec <jiamingqu0728@gmail.com>
2025-05-17 13:23:18 -0400

    Update arrays.py

--- arrays.py
+++ arrays.py
@@ -1,2 +1,4 @@
 # Python does not have built-in support for Arrays, Python "lists" can be used instead
 # To work with arrays in Python, have to import a library, like the "Numpy" library
+
+my_array = [7,12,9,4,11]
