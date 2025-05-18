1b462e2351d40ffafc015b6b9dfc042dbcdb2eeb
Jiaming Sec <jiamingqu0728@gmail.com>
2025-05-17 22:46:11 -0400

    Update arrays.py

--- arrays.py
+++ arrays.py
@@ -28,8 +28,8 @@ print('lowest value: ', minVal) # step 4
 #For each value, compare the value with the next value.
 #If the value is higher than the next one, swap the values so that the highest value comes last.
 #Go through the array as many times as there are values in the array.
-my_array = [64, 34, 25, 12, 22, 11, 90, 5]
-
+arr = [64, 34, 25, 12, 22, 11, 90, 5]
+n = len(arr)
 
 
 
