dfa6a8821eb83b78b3609babb61ccff35bb323e4
Jiaming Sec <jiamingqu0728@gmail.com>
2025-05-17 23:53:24 -0400

    Update stacks.py

--- stacks.py
+++ stacks.py
@@ -1,3 +1,3 @@
 # A stack is a data structure that can hold many elements.
-
+# when removing a pancake, it will always be the last pancake you added. This way of organizing elements is called LIFO: Last In First Out.
 
