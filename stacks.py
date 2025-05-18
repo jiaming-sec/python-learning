d24adf46c4239d59f0b19fb2cc8de615a996dcf2
Jiaming Sec <jiamingqu0728@gmail.com>
2025-05-17 23:53:58 -0400

    Update stacks.py

--- stacks.py
+++ stacks.py
@@ -1,3 +1,8 @@
 # A stack is a data structure that can hold many elements.
 # when removing a pancake, it will always be the last pancake you added. This way of organizing elements is called LIFO: Last In First Out.
 
+Push: Adds a new element on the stack.
+Pop: Removes and returns the top element from the stack.
+Peek: Returns the top element on the stack.
+isEmpty: Checks if the stack is empty.
+Size: Finds the number of elements in the stack.
