7ad6a00a74087669e641dba00191e1aa82de2228
Jiaming Sec <jiamingqu0728@gmail.com>
2025-05-18 23:32:58 -0400

    Update classes.py

--- classes.py
+++ classes.py
@@ -20,3 +20,4 @@ class Person:
   print(p1.age)
 
 # The "self" parameter is a reference to the current instance of the class, and is used to access variables that belong to the class
+# It does not have to be named self, you can call it whatever you like, but it has to be the first parameter of any function in the class
