f7275c19134e1e89aaf5f763e45aa650deccde31
Jiaming Sec <jiamingqu0728@gmail.com>
2025-05-18 22:52:39 -0400

    Update classes.py

--- classes.py
+++ classes.py
@@ -8,3 +8,4 @@ print(p1.x)
 
 # "_init_()" function
 # all classes have a function called _init(), which is always executed when the class is being initiated
+# use _init_() to ass values to object properties, or other operations that are necessary to do when the object is being created
