28e84bbf7fece6f7f89b109e19fd02b13bbacdc8
Jiaming Sec <jiamingqu0728@gmail.com>
2025-05-19 13:58:56 -0400

    Update linkedlist.py

--- linkedlist.py
+++ linkedlist.py
@@ -12,3 +12,8 @@ node4 = Node(2)
 node1.next = node2
 node2.next = node3
 node3.next = node4
+
+currentNode = node1
+while currentNode:
+  print(currentnode.data, end="->")
+  currentNode = currentNode.next
