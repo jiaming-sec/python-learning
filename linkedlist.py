86e6ccd297a9d98888a5da75c20b7121eba1150f
Jiaming Sec <jiamingqu0728@gmail.com>
2025-05-19 13:56:17 -0400

    Update linkedlist.py

--- linkedlist.py
+++ linkedlist.py
@@ -1,1 +1,5 @@
 # A linked list is a list where the nodes are linked together, each node contains data and a pointer.
+class Node:
+  def __init__(self, data):
+    self.data = data
+    self.next = None
