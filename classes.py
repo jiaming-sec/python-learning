bd58419c74a75caa16b93a0f8d95d659b52bf74a
Jiaming Sec <jiamingqu0728@gmail.com>
2025-05-18 23:36:29 -0400

    Update classes.py

--- classes.py
+++ classes.py
@@ -20,7 +20,17 @@ class Person:
   print(p1.age)
 
 # Object methods
-# Methods in objects are functions that belong to the object.             
+# Methods in objects are functions that belong to the object.  
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def myfunc(self):
+    print("Hello my name is " + self.name)
+
+p1 = Person("John", 36)
+p1.myfunc()
 # The "self" parameter is a reference to the current instance of the class, and is used to access variables that belong to the class
 # It does not have to be named self, you can call it whatever you like, but it has to be the first parameter of any function in the class
 class Person:
