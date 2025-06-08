5ac1771f0a6ec231eb0af02a6e62a786fa3cc735
Jiaming Sec <jiamingqu0728@gmail.com>
2025-06-08 12:24:18 -0400

    Update file handling.py

--- file handling.py
+++ file handling.py
@@ -2,3 +2,4 @@ The key function for working with files in Python is the open() function.
 The open() function takes two parameters; filename, and mode.
 # There are four different methods (modes) for opening a file:
 "r" - Read - Default value. Opens a file for reading, error if the file does not exist
+"a" - Append - Opens a file for appending, creates the file if it does not exist
