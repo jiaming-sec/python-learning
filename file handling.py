b39a466bdac45673104f4fb741c3ad728a049de4
Jiaming Sec <jiamingqu0728@gmail.com>
2025-06-08 13:12:41 -0400

    Update file handling.py

--- file handling.py
+++ file handling.py
@@ -85,6 +85,10 @@ with open("demofile.txt") as f:
 
 --------------------------------------------------------
 # Create a New File
+# To create a new file in Python, use the open() method, with one of the following parameters:
+# "x" - Create - will create a file, returns an error if the file exists
+# "a" - Append - will create a file if the specified file does not exists
+# "w" - Write - will create a file if the specified file does not exists
 
 
 
