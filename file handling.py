53af50d001f218355692b9ee8aa4d9ba2393bbfa
Jiaming Sec <jiamingqu0728@gmail.com>
2025-06-08 13:17:46 -0400

    Update file handling.py

--- file handling.py
+++ file handling.py
@@ -102,6 +102,15 @@ f = open("myfile.txt", "x")
 import os
 os.remove("demofile.txt")
 
+# Check if File exist:
+# To avoid getting an error, you might want to check if the file exists before you try to delete it:
+import os
+if os.path.exists("demofile.txt"):
+  os.remove("demofile.txt")
+else:
+  print("The file does not exist")
+
+
 
 
 
