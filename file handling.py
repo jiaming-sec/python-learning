95bb3a8686400fb99a2d1d704ed6741733ea196b
Jiaming Sec <jiamingqu0728@gmail.com>
2025-06-08 13:16:09 -0400

    Update file handling.py

--- file handling.py
+++ file handling.py
@@ -98,6 +98,9 @@ f = open("myfile.txt", "x")
 # Delete a File
 # To delete a file, you must import the OS module, and run its os.remove() function:
 
+# Remove the file "demofile.txt":
+import os
+os.remove("demofile.txt")
 
 
 
