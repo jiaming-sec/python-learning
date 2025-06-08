026392fc814ab0dcd945234d05151977fbbef0fe
Jiaming Sec <jiamingqu0728@gmail.com>
2025-06-08 12:24:48 -0400

    Update file handling.py

--- file handling.py
+++ file handling.py
@@ -3,3 +3,4 @@ The open() function takes two parameters; filename, and mode.
 # There are four different methods (modes) for opening a file:
 "r" - Read - Default value. Opens a file for reading, error if the file does not exist
 "a" - Append - Opens a file for appending, creates the file if it does not exist
+"w" - Write - Opens a file for writing, creates the file if it does not exist
